05fa1ac8957ace2addd6de62075c77fb7506bedb
Jerome Gozun <jerome.gozun@gmail.com>
2021-07-20 08:35:52 -0400

    Added user interface to rock paper scissors game

--- game-js.js
+++ game-js.js
@@ -1,16 +1,16 @@
 const ROCK = 'ROCK';
 const SCISSORS = 'SCISSORS';
 const PAPER = 'PAPER';
-let computerSelection = computerPlay();
+
 let playerScore = 0;
 let computerScore = 0;
 
 function computerPlay() {
   let computerChoice = Math.random();
   
-  if (computerChoice <= 0.33){
+  if (computerChoice <= 0.34){
     computerChoice = ROCK
-  } else if (computerChoice <= 0.66){
+  } else if (computerChoice <= 0.67){
     computerChoice = SCISSORS
   } else {
     computerChoice = PAPER
@@ -18,43 +18,89 @@ function computerPlay() {
   return computerChoice
 }
 
-function playerPlay(){
-  return prompt('lets play rock, paper, scissors!').toUpperCase();
-}
+const results = document.querySelector("#results")
+const currentPlayerScore = document.querySelector('#player-score')
+const currentComputerScore = document.querySelector('#computer-score')
 
 function gameRound(pChoice, cChoice){
-  
   if (pChoice === cChoice){
-    alert ('its a draw!')
-  } else if (pChoice === ROCK && cChoice === SCISSORS){
-    alert(`you chose ${ROCK}, and the computer chose ${SCISSORS}. You win!`), playerScore++
+    results.textContent = 'its a draw!'
+  }else if (pChoice === ROCK && cChoice === SCISSORS){
+    results.textContent = `you chose ${ROCK}, and the computer chose ${SCISSORS}. You win!`, 
+    playerScore++,
+    currentPlayerScore.textContent = playerScore;
   }else if (pChoice === SCISSORS && cChoice === ROCK){
-    alert(`you chose ${SCISSORS} and the computer chose ${ROCK}. You win!`), playerScore++
+    results.textContent = `you chose ${SCISSORS} and the computer chose ${ROCK}. You win!`, 
+    playerScore++,
+    currentPlayerScore.textContent = playerScore;
   }else if (pChoice === PAPER && cChoice === ROCK){
-    alert(`you chose ${PAPER} and the computer chose ${ROCK}. You win!`), playerScore++
-  } else{
-    alert ('you lose!'), computerScore++
+    results.textContent = `you chose ${PAPER} and the computer chose ${ROCK}. You win!`, 
+    playerScore++,
+    currentPlayerScore.textContent = playerScore;
+  }else{
+    results.textContent = 'you lose!', 
+    computerScore++,
+    currentComputerScore.textContent = computerScore;
   }
-  return
+  playAgainHandler()
 }
 
-function gameResult(){
-    for (let i = 0; i < 5; i++){
-    gameRound(playerPlay(), computerPlay());
-    alert(`Computer Score : ${computerScore} Player Score: ${playerScore}`);
-  }
-  console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`); 
-  if (playerScore > computerScore){
-    console.log('You won the Game!')
-  }else if (computerScore > playerScore){
-    console.log('You lost the Game!')
-  }else {
-    console.log('Tie Game!')
+let playAgain = document.querySelector("#play-again")
+
+function playAgainHandler(){
+  if (computerScore === 5 || playerScore === 5){
+    playAgain.textContent = "Play Again?",
+    rockSelect.removeEventListener('click', chooseRock),
+    paperSelect.removeEventListener('click', choosePaper),
+    scissorsSelect.removeEventListener('click', chooseScissors)
   }
 }
 
+function resetRound(){
+  currentPlayerScore.textContent = 0;
+  currentComputerScore.textContent = 0;
+  computerScore = 0;
+  playerScore = 0;
+  results.textContent = "";
+  playAgain.textContent = "";
+  rockSelect.addEventListener('click', chooseRock);
+  paperSelect.addEventListener('click', choosePaper);
+  scissorsSelect.addEventListener('click', chooseScissors);
+}
+
+playAgain.addEventListener('click', resetRound)
+
+
+const rockSelect = document.querySelector("#rock-img");
+const paperSelect = document.querySelector("#paper-img");
+const scissorsSelect = document.querySelector("#scissors-img");
+
+function chooseRock(){
+  let pChoice = ROCK;
+  let cChoice = computerPlay();
+  gameRound(pChoice, cChoice);
+}
+
+rockSelect.addEventListener('click', chooseRock);
+
+function choosePaper(){
+  let pChoice = PAPER
+  let cChoice = computerPlay();
+  gameRound(pChoice, cChoice);
+}
+
+paperSelect.addEventListener('click', choosePaper);
+
+function chooseScissors(){
+  let pChoice = SCISSORS;
+  let cChoice = computerPlay();
+  gameRound(pChoice, cChoice)
+}
+
+scissorsSelect.addEventListener('click', chooseScissors)
 
 
 
 
-gameResult()
+
+
